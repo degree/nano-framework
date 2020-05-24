8793653c4ed071120010ec5c81734a8dc34b4b08
Eugene Dubrovka <edubrovka@apollon.de>
2020-05-24 12:25:50 +0200

    break test

--- src/test/java/by/degree/learn/solid/framework/JavaConfigTest.java
+++ src/test/java/by/degree/learn/solid/framework/JavaConfigTest.java
@@ -42,6 +42,6 @@ class JavaConfigTest {
     @Test
     void classIsListedOnce() {
         var classes = config.listImplementations(TestSingleImplementationInterface.class);
-        assertEquals(1, classes.size());
+        assertEquals(2, classes.size());
     }
 }
